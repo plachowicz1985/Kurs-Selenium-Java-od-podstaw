f06209c7a1ab0edda1f4468c561bca487affb9c1
plachowicz1985 <piotr.lachowicz1985@gmail.com>
2023-02-02 22:42:18 +0100

    commit 31.01 Xpath selectors

--- ProjectIdea/src/test/java/XpathSelectorsTest.java
+++ ProjectIdea/src/test/java/XpathSelectorsTest.java
@@ -34,18 +34,56 @@ public class XpathSelectorsTest {
 
         By partialLink = By.xpath("//a[contains(text(), 'Visit')]");
         driver.findElement(partialLink);
-//
-//        By cssId = By.cssSelector("#clickOnMe");
-//        driver.findElement(cssId);
-//
-//        By cssClass = By.cssSelector(".topSecret");
-//        driver.findElement(cssClass);
-//
-//        By cssTag = By.cssSelector("input");
-//        driver.findElement(cssTag).sendKeys("Pierwszy");
-//
-//        By cssName = By.cssSelector("[name='fname']");
-//        driver.findElement(cssName);
+
+        By allXpath = By.xpath("//*");
+        driver.findElement(allXpath);
+
+        By secondElement = By.xpath("(//input)[2]");
+        driver.findElement(secondElement);
+
+        By lastElement = By.xpath("(//input)[last()]");
+        driver.findElement(lastElement);
+
+        By elementWithAttribute = By.xpath("//*[@name]");
+        driver.findElement(elementWithAttribute);
+
+        By attrEq = By.xpath("//button[@id='clickOnMe']");
+        By attrNotEq = By.xpath("//*[@id!='clickOnMe']");
+        By attrCont = By.xpath("//*[contains(@name, 'ame')]");
+        By startsWith = By.xpath("//*[starts-with(@name, 'user')]");
+        //By endsWith = By.xpath("//*[ends-with(@name, 'name')]");
+
+        driver.findElement(attrEq);
+        driver.findElement(attrNotEq);
+        driver.findElement(attrCont);
+        driver.findElement(startsWith);
+        //driver.findElement(endsWith);
+
+        By child = By.xpath("//div/child::ul");
+        By desc = By.xpath("//div/descendant::ul");
+        By parent = By.xpath("//div/../..");
+        By asc = By.xpath("//div/ancestor::*");
+        By fall = By.xpath("//img//following::*");
+        By fallSib = By.xpath("//img//following-sibling::*");
+        By pre = By.xpath("//img/preceding::*");
+        By preSib = By.xpath("//img/preceding-sibling::*");
+
+        driver.findElement(child);
+        driver.findElement(desc);
+        driver.findElement(parent);
+        driver.findElement(asc);
+        driver.findElement(fall);
+        driver.findElement(fallSib);
+        driver.findElement(pre);
+        driver.findElement(preSib);
+
+        By divsAndLinks = By.xpath("//a | //div");
+        By andOp = By.xpath("//input[@name='fname' and @id='fname']");
+        By orOp = By.xpath("//input[@name='fname' or @id='fname']");
+
+        driver.findElement(divsAndLinks);
+        driver.findElement(andOp);
+        driver.findElement(orOp);
 //
 //        By cssClass2 = By.cssSelector("[class='topSecret']");
 //        driver.findElement(cssClass2);
