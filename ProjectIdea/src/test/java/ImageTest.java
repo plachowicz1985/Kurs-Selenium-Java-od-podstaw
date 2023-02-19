ac73b1468b04e37ae318a47285ec2ad0246b9667
plachowicz1985 <piotr.lachowicz1985@gmail.com>
2023-02-19 16:54:13 +0100

    commit 19.02 Wait Test

---
+++ ProjectIdea/src/test/java/ImageTest.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ImageTest {
+
+    WebDriver driver;
+
+    @Test
+    public void imageTest() {
+
+        driver = new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+
+        WebElement image = driver.findElement(By.tagName("img"));
+        String height = image.getAttribute("naturalHeight");
+
+        Assert.assertEquals("0", height);
+
+    }
+
+}
